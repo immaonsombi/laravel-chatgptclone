dec417778b5dc38ecfae69f49d3a7a8da21ff2f2
immaonsombi <immanuel.onsombi@strathmore.edu>
2023-12-01 10:12:14 +0300

    Initial commit

---
+++ resources/js/Layouts/ChatLayout.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+<template >
+    <aside class="ml-[-100%] fixed z-10 top-0 pb-3 px-4 w-full flex flex-col justify-between h-screen bo">
+        <div>
+            <slot name="aside" />
+
+        </div>
+    </aside>
+    <div class="ml-auto mb-6 lg:w[75%]">
+
+        <div class="pt-6">
+            <main>
+                <slot />
+            </main>
+        </div>
+    </div>
+</template>
