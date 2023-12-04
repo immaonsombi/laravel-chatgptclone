96a55c7dbec9d3e239ecd5c39a5d7d009d11b3d1
immaonsombi <immanuel.onsombi@strathmore.edu>
2023-12-04 11:35:09 +0300

    completed

---
+++ app/Http/Controllers/ChatGptDestroyController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chat;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+
+class ChatGptDestroyController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Chat $chat): RedirectResponse
+    {
+        $chat->delete();
+        return to_route('chat.show');
+    }
+}
